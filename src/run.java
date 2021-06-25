f019799359c0c04e51977d38fa2092766f34f089
Sze Yuan <55387911+LeeSzeYuan@users.noreply.github.com>
2021-06-25 23:24:35 +0800

    Add files via upload

---
+++ src/run.java
@@ -0,0 +1,19 @@
+package src;
+import javax.swing.*;
+
+public class run{
+    static private int in;
+
+	public static void main(String[] args) {
+        String start = "Wanna play a game?";
+        String[] options = {"pacman", "snake"};
+        in = JOptionPane.showOptionDialog(null, start, "Methods to draw a line graph.", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+        
+        if(in == 0){
+            new Pacman();
+        }else if(in == 1){
+            System.out.print("Snake");
+        }
+		
+	}
+}
