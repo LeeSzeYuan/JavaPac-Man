b717c621e5c9711a349877b13fa23fc28b591963
lohyewchong <75685972+yewchong2207@users.noreply.github.com>
2021-06-26 16:16:52 +0800

    Update SnakeGamePanel.java

--- GameStimulator/SnakeGamePanel.java
+++ GameStimulator/SnakeGamePanel.java
@@ -5,14 +5,14 @@ import java.util.Random;
 
 public class SnakeGamePanel extends JPanel implements ActionListener{
 
-	static final int SCREEN_WIDTH = 320;//1300
-	static final int SCREEN_HEIGHT = 420;//750
+	static final int SCREEN_WIDTH = 320;
+	static final int SCREEN_HEIGHT = 420;
 	static final int UNIT_SIZE = 20; // the size of the item 
 	static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/(UNIT_SIZE*UNIT_SIZE); // the number of item that can be fit in the panel
 	static final int DELAY = 175; // this will affect the speed of the snake 
 	final int x[] = new int[GAME_UNITS]; // x,y hold xand y coordinate of the body fo the snake 
 	final int y[] = new int[GAME_UNITS];
-	int bodyParts = 3;
+	int bodyLength = 3;
 	int foodEaten;
 	int foodX;
 	int foodY;
@@ -33,34 +33,38 @@ public class SnakeGamePanel extends JPanel implements ActionListener{
 	public void startGame() {
 		newFood();
 		running = true;
-		timer = new Timer(DELAY,this);
-		timer.start();
+		timer = new Timer(DELAY,this);  // this is not from java.util.Timer , its java.swing.Timer
+										// the "this" is refer to the ActionListener and an initial listener 
+										// the "DELAY" is the milliseconds for the initial and between-event delay
+		
+		timer.start(); //Starts the Timer, causing it to start sending action events to its listeners. 
 	}
-	public void paintComponent(Graphics gc) {
-		super.paintComponent(gc);
+
+	public void paintComponent(Graphics gc) { // exists in a JPanel class , uses to draw food and snake and even background color
+		super.paintComponent(gc); // use super to add Graphic Obeject into the mehtod 
 		draw(gc);
 	}
+
 	public void draw(Graphics g) {
 		
 		if(running) {// if the program running
 			g.setColor(Color.red);
 			g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE); // set the property of the food eg. its size and shape
 		
-			for(int i = 0; i< bodyParts;i++) {
+			for(int i = 0; i< bodyLength;i++) {
 				if(i == 0) { // it is for the head of snake 
 					g.setColor(Color.green);
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
 				}
 				else { // other part of the snake 
 					g.setColor(new Color(45,180,0));
-					//g.setColor(new Color(random.nextInt(255),random.nextInt(255),random.nextInt(255)));
 					g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE); // fill the snake in rect and set i property 
 				}			
 			}
 			//score display 
 			g.setColor(Color.white);
 			g.setFont( new Font("Times New Roman",Font.BOLD, 20));
-			FontMetrics m = getFontMetrics(g.getFont()); 
+			FontMetrics m = getFontMetrics(g.getFont());  // Gets the font metrics of the current font.
 			g.drawString("Score: "+foodEaten, (SCREEN_WIDTH - m.stringWidth("Score: "+foodEaten))/2, g.getFont().getSize()); // display the score 
 		}
 		else {
@@ -69,41 +73,41 @@ public class SnakeGamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void newFood(){
-		foodX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE; // we * with unit size to make the food place evenly in the panel 
+		foodX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;  // we * with unit size to make the food place evenly in the panel 
 		foodY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE; // we still cast int ,just to make it does not cause any error 
 	}
 	public void move(){ //to move the snake 
-		for(int i = bodyParts;i>0;i--) { 
+		for(int i = bodyLength ;i>0;i--) { 
 			x[i] = x[i-1]; // the x coordinate of the index before it will be assign to it 
 			y[i] = y[i-1];
 		}
 		
 		switch(direction) {
 		case 'U':
-			y[0] = y[0] - UNIT_SIZE;
+			y[0] = y[0] - UNIT_SIZE; // if snake move up , the y coordinate increase by 1 
 			break;
 		case 'D':
-			y[0] = y[0] + UNIT_SIZE;
+			y[0] = y[0] + UNIT_SIZE; // if snake move down , the y coordinate decrease by 1 
 			break;
 		case 'L':
-			x[0] = x[0] - UNIT_SIZE;
+			x[0] = x[0] - UNIT_SIZE; // if snake move left , the x coordinate decrease by 1 
 			break;
 		case 'R':
-			x[0] = x[0] + UNIT_SIZE;
+			x[0] = x[0] + UNIT_SIZE; // if snake move right , the x coordinate increase by 1 
 			break;
 		}
 		
 	}
 	public void checkFood() {
 		if((x[0] == foodX) && (y[0] == foodY)) { // when the head is at the postion of the food 
-			bodyParts++; // increase bodypart by 1 
+			bodyLength++; // increase bodylength by 1 
 			foodEaten++;
 			newFood(); // call to create new food 
 		}
 	}
 	public void checkCollisions() {
 		//checks whether head collides with body
-		for(int i = bodyParts;i>0;i--) {
+		for(int i = bodyLength;i>0;i--) {
 			if((x[0] == x[i])&& (y[0] == y[i])) {
 				running = false;
 			}
@@ -144,14 +148,14 @@ public class SnakeGamePanel extends JPanel implements ActionListener{
 		g.drawString("Game Over", (SCREEN_WIDTH - m2.stringWidth("Game Over"))/2, SCREEN_HEIGHT/2);
 	}
 	@Override
-	public void actionPerformed(ActionEvent a) {
+	public void actionPerformed(ActionEvent a) { // this method invoked automatically whenever the component is created 
 		
 		if(running) {
 			move();
 			checkFood();
 			checkCollisions();
 		}
-		repaint();// if the game is not running, call repaint method to repaint the panel
+		repaint();// if the game is not running, call repaint method to redraw the panel 
 	}
 	
 	public class MyKeyAdapter extends KeyAdapter{
@@ -203,16 +207,6 @@ public class SnakeGamePanel extends JPanel implements ActionListener{
 									}
 									break;        
 			
-			case KeyEvent.VK_ESCAPE : if(running){
-				running = false;
-				timer.stop();
-			  }
-			  break;
-			case KeyEvent.VK_SPACE : if(!running){
-				running = true;
-				startGame();//start game
-			  }
-			  break;
 			}
 		}
 	}
